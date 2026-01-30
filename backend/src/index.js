b9b41a71a09721a9a170efa85b44d5ec9e042bc8
Vitor Cardoso <vitorsac13@gmail.com>
2026-01-30 14:39:17 -0300

    setup commit

---
+++ backend/src/index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import cors from 'cors'
+import passport from 'passport'
+import { Mongo } from './database/mongo.js'
+import { config } from 'dotenv'
+import authRouter from './auth/auth.js'
+import usersRouter from './routes/users.js'
+
+config()
+
+async function main () {
+    const hostname = 'localhost'
+    const port = 3000
+    //Express
+    const app = express()
+    
+    const mongoConnection = await Mongo.connect({ mongoConnectionString: process.env.MONGO_CS, mongoDbName: process.env.MONGO_DB_NAME})
+    console.log(mongoConnection)
+    //Inicialização
+    app.use(express.json())
+    app.use(cors())
+    app.use(passport.initialize())
+
+    app.get('/', (req, res) => {
+        res.send({
+            success: true,
+            statuscode: 200,
+            body: 'Welcome to Clínica!'
+        })
+    })
+
+    app.use('/auth', authRouter)
+    app.use('/users', usersRouter)
+
+    app.listen(port, () => {
+        console.log(`Server running on http://${hostname}:${port}`)
+    })
+}
+
+main ()
