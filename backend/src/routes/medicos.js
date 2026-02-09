285fe4f6e4d16383004e75e00489c416321159f0
Vitor Cardoso <vitorsac13@gmail.com>
2026-02-09 17:51:48 -0300

    Update router Medico

--- backend/src/routes/medicos.js
+++ backend/src/routes/medicos.js
@@ -1,25 +1,36 @@
-import express from 'express'
-import AgendamentoController from '../controllers/medicoController.js'
+import express from "express"
+import MedicoController from "../controllers/medicoController.js"
+import authMiddleware from "../middlewares/authMiddleware.js"
+import adminMiddleware from "../middlewares/adminMiddleware.js"
 
 const medicosRouter = express.Router()
-
 const medicoController = new MedicoController()
 
-medicosRouter.get('/', async (req, res) => {
+// LISTAR MÉDICOS
+medicosRouter.get("/", authMiddleware, async (req, res) => {
     const { success, statusCode, body } = await medicoController.getMedicos()
-
-    res.status(statusCode).send({ success, statusCode, body })
+    res.status(statusCode).json({ success, statusCode, body })
 })
 
-medicosRouter.delete('/:id', async (req, res) => {
-    const { success, statusCode, body } = await medicoController.deleteMedico(req.params.id)
-    res.status(statusCode).send({ success, statusCode, body })
+
+// CRIAR MÉDICO (ADMIN ONLY)
+medicosRouter.post("/", authMiddleware, adminMiddleware, async (req, res) => {
+    const { success, statusCode, body } = await medicoController.createMedico(req.body)
+    res.status(statusCode).json({ success, statusCode, body })
 })
 
-medicosRouter.put('/:id', async (req, res) => {
+
+// ATUALIZAR MÉDICO (ADMIN ONLY)
+medicosRouter.put("/:id", authMiddleware, adminMiddleware, async (req, res) => {
     const { success, statusCode, body } = await medicoController.updateMedico(req.params.id, req.body)
-    res.status(statusCode).send({ success, statusCode, body })
+    res.status(statusCode).json({ success, statusCode, body })
 })
 
+
+// DELETAR MÉDICO (ADMIN ONLY)
+medicosRouter.delete("/:id", authMiddleware, adminMiddleware, async (req, res) => {
+    const { success, statusCode, body } = await medicoController.deleteMedico(req.params.id)
+    res.status(statusCode).json({ success, statusCode, body })
+})
 
 export default medicosRouter
