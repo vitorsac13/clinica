f17db55aef3a5c14e8ac2ae41ae9b3768778ef5e
Vitor Cardoso <vitorsac13@gmail.com>
2026-02-09 17:56:49 -0300

    Commit DAO Medico

--- backend/src/dao/medicoDAO.js
+++ backend/src/dao/medicoDAO.js
@@ -1,24 +1,67 @@
 import { Mongo } from "../database/mongo.js"
-import { ObjectId } from 'mongodb'
-import crypto from 'crypto'
+import { ObjectId } from "mongodb"
+import crypto from "crypto"
 
-const collectionName = 'medico'
+const collectionName = "medico"
 
 export default class MedicoDAO {
 
-    async getMedicos(){
-        const result = await Mongo.db.collection(collectionName).find({}).toArray()
+  async getMedicos() {
+    const result = await Mongo.db
+      .collection(collectionName)
+      .find({})
+      .toArray()
 
-        return result
+    return result
+  }
+
+  async createMedico(medicoData) {
+
+    // Gera ID público (opcional)
+    const medico = {
+      _id: new ObjectId(),
+      nome: medicoData.nome,
+      especialidade: medicoData.especialidade,
+      crm: medicoData.crm,
+      email: medicoData.email || null,
+      createdAt: new Date(),
+      updatedAt: new Date()
     }
 
-    async deleteMedico(medicoId){
-        const result = await Mongo.db.collection(collectionName).findOneAndDelete({ _id: new ObjectId(medicoId) })
-        return result
+    const result = await Mongo.db
+      .collection(collectionName)
+      .insertOne(medico)
+
+    return {
+      acknowledged: result.acknowledged,
+      insertedId: result.insertedId,
+      medico
+    }
+  }
+
+  async updateMedico(medicoId, medicoData) {
+
+    const updateData = {
+      ...medicoData,
+      updatedAt: new Date()
     }
 
-    async updateMedico(medicoId, medicoData){
-                
-    }
+    const result = await Mongo.db
+      .collection(collectionName)
+      .findOneAndUpdate(
+        { _id: new ObjectId(medicoId) },
+        { $set: updateData },
+        { returnDocument: "after" } // retorna documento atualizado
+      )
 
+    return result
+  }
+
+  async deleteMedico(medicoId) {
+    const result = await Mongo.db
+      .collection(collectionName)
+      .findOneAndDelete({ _id: new ObjectId(medicoId) })
+
+    return result
+  }
 }
