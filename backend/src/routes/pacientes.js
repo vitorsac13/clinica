e55d2e9a3fddcf902544bb87bc59a99b83ee8387
Vitor Cardoso <vitorsac13@gmail.com>
2026-02-09 18:03:29 -0300

    Update router Paciente

--- backend/src/routes/pacientes.js
+++ backend/src/routes/pacientes.js
@@ -1,25 +1,35 @@
 import express from 'express'
-import AgendamentoController from '../controllers/pacienteController.js'
+import PacienteController from '../controllers/pacienteController.js'
 
 const pacientesRouter = express.Router()
 
 const pacienteController = new PacienteController()
 
-pacientesRouter.get('/', async (req, res) => {
+// LISTAR PACIENTES
+pacientesRouter.get("/", authMiddleware, async (req, res) => {
     const { success, statusCode, body } = await pacienteController.getPacientes()
-
-    res.status(statusCode).send({ success, statusCode, body })
+    res.status(statusCode).json({ success, statusCode, body })
 })
 
-pacientesRouter.delete('/:id', async (req, res) => {
-    const { success, statusCode, body } = await pacienteController.deletePaciente(req.params.id)
-    res.status(statusCode).send({ success, statusCode, body })
+
+// CRIAR PACIENTE (ADMIN ONLY)
+pacientesRouter.post("/", authMiddleware, adminMiddleware, async (req, res) => {
+    const { success, statusCode, body } = await pacienteController.createPaciente(req.body)
+    res.status(statusCode).json({ success, statusCode, body })
 })
 
-pacientesRouter.put('/:id', async (req, res) => {
+
+// ATUALIZAR PACIENTE (ADMIN ONLY)
+pacientesRouter.put("/:id", authMiddleware, adminMiddleware, async (req, res) => {
     const { success, statusCode, body } = await pacienteController.updatePaciente(req.params.id, req.body)
-    res.status(statusCode).send({ success, statusCode, body })
+    res.status(statusCode).json({ success, statusCode, body })
 })
 
+
+// DELETAR PACIENTE (ADMIN ONLY)
+pacientesRouter.delete("/:id", authMiddleware, adminMiddleware, async (req, res) => {
+    const { success, statusCode, body } = await pacienteController.deletePaciente(req.params.id)
+    res.status(statusCode).json({ success, statusCode, body })
+})
 
 export default pacientesRouter
