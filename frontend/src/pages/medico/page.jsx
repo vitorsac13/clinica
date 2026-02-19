7b79430e02959365f3905b319b35adddbc8d115f
Vitor Cardoso <vitorsac13@gmail.com>
2026-02-19 15:28:15 -0300

    fix page Medico

--- frontend/src/pages/medico/page.jsx
+++ frontend/src/pages/medico/page.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
-import styles from "./medicos.module.css"
+import styles from "./page.module.css"
 
 export default function Medico() {
   
